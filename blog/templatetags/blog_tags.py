dd52fe13e0ef3fe43091bf07b3d6cf5bc5e2458d
benq <511648108@qq.com>
2017-06-23 23:31:43 +0800

    add 标签yun

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,5 +1,6 @@
-from ..models import Post, Category
+from ..models import Post, Category, Tag
 from django import template
+from django.db.models.aggregates import Count
 register = template.Library()
 
 @register.simple_tag
@@ -14,4 +15,9 @@ def archives():
 @register.simple_tag
 def get_categories():
     # 别忘了在顶部引入 Category 类
-    return Category.objects.all()
+    #return Category.objects.all()
+    return Category.objects.annotate(num_posts=Count('post')).filter(num_posts__gt=0)
+
+@register.simple_tag
+def get_tags():
+    return Tag.objects.annotate(num_posts=Count('post')).filter(num_posts__gt=0)
